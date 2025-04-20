fea73a3172030a411faabda0bebe1fd984c9a136
Sarvarbek <ismatovsarvar76@gmail.com>
2025-04-20 18:54:26 +0500

    s

---
+++ index.js
@@ -0,0 +1,27 @@
+const key = "s6KSKKPYFtvENjKp9mJOa6he9yptGJHDb2_UybFqAc4";
+const base_url = `https://api.unsplash.com/photos/?client_id=${key}`;
+const gallery = document.querySelector(".gallery-grid");
+window.addEventListener("DOMContentLoaded", () => {
+  fetchData();
+});
+
+async function fetchData() {
+  const data = await fetch(base_url);
+  const res = await data.json();
+  getCards(res);
+}
+function getCards(cards) {
+  gallery.innerHTML = "";
+  cards.forEach((card) => {
+    displayCard(card, card.id);
+  });
+}
+function displayCard(data, id) {
+  const div = document.createElement("div");
+  div.setAttribute("class", "card");
+  div.innerHTML = `
+            <img src="${data.urls.small}" alt="Изображение 1" class="card-image" data-id="${id}" />
+            <h3 class="card-title">${data.alt_description}</h3>
+  `;
+  gallery.appendChild(div);
+}
---
+++ modal.js
@@ -0,0 +1,34 @@
+const modalContainer = document.querySelector(".modal-container");
+const modal = document.querySelector(".modal");
+const cardTap = document.querySelector(".card-image");
+
+gallery.addEventListener("click", (e) => {
+  if (e.target.classList.contains("card-image")) {
+    const id = e.target.dataset.id;
+    modalContainer.classList.add("open");
+    openModal(id);
+  }
+});
+
+async function openModal(id) {
+  const data = await fetch(
+    `https://api.unsplash.com/photos/${id}?client_id=${key}`
+  );
+  const res = await data.json();
+  const div = document.createElement("div");
+  div.setAttribute("class", "modal");
+  div.innerHTML = `
+   <img
+          src="${res.urls.full}"
+          class="modal-image"
+          id="modalImage"
+        />
+        <h3 class="modal-title" id="modalTitle">${res.alt_description}</h3>
+  `;
+  modalContainer.appendChild(div);
+  const close = document.querySelector(".modal-close");
+  close.addEventListener("click", () => {
+    modalContainer.classList.remove("open");
+    div.remove();
+  });
+}
